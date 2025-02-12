36cc0ac82ee63c81c0612dea1c935fc625821030
Moreno BRUSCHI <moreno.bruschi@freseniusmedicalcare.com>
2025-02-12 20:57:11 +0100

    Better menu

--- ui/tailwind.config.js
+++ ui/tailwind.config.js
@@ -1,9 +1,26 @@
+// tailwind.config.js
 module.exports = {
   content: [
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: 'var(--primary-color)',
+          dark: 'var(--primary-dark)' // Add this line
+        },
+        secondary: 'var(--secondary-color)',
+        accent: 'var(--accent-color)',
+        success: 'var(--success-color)',
+        error: 'var(--error-color)',
+        background: 'var(--background-color)',
+        text: 'var(--text-color)',
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
